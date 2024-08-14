d12113fe1fb7001adb2c9b3dc735f3e969b8eb8d
TK-2912 <tanay.kulkarni2019@gmail.com>
2024-08-14 10:04:53 +0530

    Initial Commit

---
+++ next_user_auth-initial/app/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Mulish } from "next/font/google";
+
+const mulish = Mulish({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Solve It Out",
+  description: "Next Authentication",
+};
+
+export default function RootLayout({children,}: {children: React.ReactNode;}) 
+{
+  return (
+    <html lang="en">
+      <body
+        suppressContentEditableWarning
+        suppressHydrationWarning
+        className={mulish.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
